f0b9bc8ee9ca3f6602111cdc32b0f239af2fed3e
minhnhat97kg <minhnhat97kg@gmail.com>
2025-11-13 21:01:47 +0700

    fix: android

--- flake.nix
+++ flake.nix
@@ -426,23 +426,88 @@
                 useGlobalPkgs = true;
                 config =
                   { config, pkgs, ... }:
-                  nixpkgs.lib.mkMerge [
-                    (sharedHomeConfig { inherit pkgs; })
-                    {
-                      # Note: home.username and home.homeDirectory are automatically
-                      # managed by nix-on-droid and should not be set here
+                  {
+                    # Note: home.username and home.homeDirectory are automatically
+                    # managed by nix-on-droid and should not be set here
+                    home.stateVersion = "24.05";
 
-                      # Android-specific shell configuration
-                      programs.zsh.initExtra = ''
-                        # Android-specific paths
-                        export GOPATH=$HOME/go
-                        export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+                    # Allow unfree packages
+                    nixpkgs.config.allowUnfree = true;
 
-                        # Termux-specific
-                        export TMPDIR=/data/data/com.termux.nix/files/usr/tmp
-                      '';
-                    }
-                  ];
+                    # Shared packages
+                    home.packages = sharedPackages pkgs;
+
+                    # Programs configuration
+                    programs = {
+                      neovim = {
+                        enable = true;
+                        defaultEditor = true;
+                      };
+
+                      tmux = {
+                        enable = true;
+                        keyMode = "vi";
+                        mouse = true;
+                        plugins = with pkgs.tmuxPlugins; [
+                          better-mouse-mode
+                          yank
+                          {
+                            plugin = catppuccin;
+                            extraConfig = ''
+                              set -g default-terminal "tmux-256color"
+                              set-option -ga terminal-overrides ",xterm-256color:Tc"
+                              set -g @catppuccin_flavor "mocha"
+                              set -g @catppuccin_window_status_style "rounded"
+                            '';
+                          }
+                        ];
+                        extraConfig = ''
+                          set -sg escape-time 0
+                          setw -g mode-keys vi
+                          bind-key -T copy-mode-vi v send-keys -X begin-selection
+                          bind c new-window -c "#{pane_current_path}"
+                          bind | split-window -h -c "#{pane_current_path}"
+                          bind - split-window -v -c "#{pane_current_path}"
+
+                          # Vim-like pane navigation
+                          is_vim="ps -o state= -o comm= -t '#{pane_tty}' | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
+                          bind -n C-h run "($is_vim && tmux send-keys C-h) || tmux select-pane -L"
+                          bind -n C-j run "($is_vim && tmux send-keys C-j) || tmux select-pane -D"
+                          bind -n C-k run "($is_vim && tmux send-keys C-k) || tmux select-pane -U"
+                          bind -n C-l run "($is_vim && tmux send-keys C-l) || tmux select-pane -R"
+                        '';
+                      };
+
+                      zsh = {
+                        enable = true;
+                        initExtra = ''
+                          export GOPATH=$HOME/go
+                          export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+                          # Termux-specific
+                          export TMPDIR=/data/data/com.termux.nix/files/usr/tmp
+                        '';
+                        shellAliases = {
+                          ll = "ls -l";
+                          lg = "lazygit";
+                          e = "nvim";
+                        };
+                        oh-my-zsh = {
+                          enable = true;
+                          theme = "robbyrussell";
+                        };
+                      };
+
+                      lazygit.enable = true;
+                      direnv.enable = true;
+                    };
+
+                    # Neovim config
+                    home.file.".config/nvim/" = {
+                      source = ./nvim;
+                      recursive = true;
+                    };
+                  };
               };
             }
           ];
